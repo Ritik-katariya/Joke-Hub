f28417c928a489712adb4b5b710c45b0b9858212
Ritik-katariya <153073623+Ritik-katariya@users.noreply.github.com>
2025-08-26 10:57:11 +0530

    Remove unused imports from layout and page components for cleaner code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import JokesPage from "@/components/jokes-page/JokesPage";
-import Jokes from "@/components/jokes-page/JokesPage";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center text-4xl font-bold bg-gray-900 text-white pt-4">
